a36ae3f684c3781b1dbe2fa3e416e7c06802702a
Andrii-Chemerys <108608082+Andrii-Chemerys@users.noreply.github.com>
2024-10-31 00:34:20 +0200

    encoding features

--- src/lending_club/pipelines/encode/nodes.py
+++ src/lending_club/pipelines/encode/nodes.py
@@ -3,6 +3,30 @@ This is a boilerplate pipeline 'encode'
 generated using Kedro 0.19.9
 """
 import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
 
-def _parse_emp_len(x: pd.Series) -> pd.Series:
-    return x.str.split(" ").str[0].str.replace("+", "").str.replace("<", "0").astype(int)
+# Function to encode emp_length to number
+def _parse_emp_len(df: pd.DataFrame, params: dict) -> pd.DataFrame:
+    df[params['emp_len']] = df[params['emp_len']].str.split(" ").str[0].str.replace("+", "").str.replace("<", "0").astype(int)
+    return df
+
+# Function to encode default_status from loan_status
+def _default_status(df: pd.DataFrame, params: dict) -> pd.DataFrame:
+    df['default_status'] = (
+            (df[params['default']] == 'Charged Off') |
+            (df[params['default']] == 'Does not meet the credit policy. Status:Charged Off') |
+            (df[params['default']] == 'Default')
+    )
+    return df.drop(params['default'], axis=1)
+
+# Function encodes object values to numbers
+def _encode(df: pd.DataFrame, params: dict) -> pd.DataFrame:
+    encoder = OrdinalEncoder()
+    df[params['category']] = encoder.fit_transform(df[params['category']])
+    return df
+
+def encode_dataset(df: pd.DataFrame, params: dict) -> pd.DataFrame:
+    df = _parse_emp_len(df, params)
+    df = _default_status(df, params)
+    df = _encode(df, params)
+    return df
--- src/lending_club/pipelines/encode/pipeline.py
+++ src/lending_club/pipelines/encode/pipeline.py
@@ -3,8 +3,14 @@ This is a boilerplate pipeline 'encode'
 generated using Kedro 0.19.9
 """
 
-from kedro.pipeline import Pipeline, pipeline
-
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import encode_dataset
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=encode_dataset,
+            inputs=['primary_lc_dataset', 'parameters'],
+            outputs='primary_lc_dataset_encoded',
+        )
+    ])
