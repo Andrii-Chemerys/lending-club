753949ea36296c6e985106d369b5e91cb15461e1
Andrii Chemerys <avchemerys@gmail.com>
2024-10-30 17:31:10 +0200

    encode development

--- src/lending_club/pipelines/encode/nodes.py
+++ src/lending_club/pipelines/encode/nodes.py
@@ -2,3 +2,7 @@
 This is a boilerplate pipeline 'encode'
 generated using Kedro 0.19.9
 """
+import pandas as pd
+
+def _parse_emp_len(x: pd.Series) -> pd.Series:
+    return x.str.split(" ").str[0].str.replace("+", "").str.replace("<", "0").astype(int)
